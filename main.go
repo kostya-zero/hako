1de9e2374b5a1601ba07245702850045ab29d0f1
Konstantin Zhigaylo <zero@kostyazero.com>
2026-01-27 12:46:52 +0300

    refactor: remove cli.go

--- main.go
+++ main.go
@@ -3,11 +3,48 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	cli := BuildCli()
-	if err := cli.Execute(); err != nil {
+	var configPath string
+
+	rootCmd := &cobra.Command{
+		Use:   "hako",
+		Short: "A key-value storage over HTTP.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Nothing to do. Use `hako --help` for help.")
+		},
+	}
+
+	runCmd := &cobra.Command{
+		Use:   "run",
+		Short: "Start the server.",
+		Run: func(cmd *cobra.Command, args []string) {
+			PrepareLogger()
+			var config Config
+
+			if configPath != "" {
+				result, err := LoadConfig(configPath)
+				if err != nil {
+					fmt.Printf("Failed to parse configuration file: %s\n", err.Error())
+					os.Exit(1)
+				}
+				config = result
+			} else {
+				config = GetDefaultConfig()
+			}
+
+			StartServer(&config)
+		},
+	}
+
+	runCmd.Flags().StringVar(&configPath, "config", "", "A path to configuration file")
+
+	rootCmd.AddCommand(runCmd)
+
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Printf("An error occured in CLI interactions")
 		os.Exit(1)
 	}
